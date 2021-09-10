b3862aec7a54086429ade13b6ab3f8fdeae4dfa9
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-10 14:37:05 +0300

    setup for output management

--- src/index.js
+++ src/index.js
@@ -1,40 +1,17 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
 
-  element.appendChild(myIcon);
+  btn.onclick = printMe;
 
-  // the way I get these files is also useful for data visualisation too, 
-  // instead of making an ajax request and parsing the data at runtime you 
-  // can load it into your module during the build process so that the parsed 
-  // data is ready to go as soon as the module hits the browser.
-  // apparently only the default export of JSON modules can be used without warning.
-  // so no {} imports, it will throw an error
-  console.log(Notes)
-  console.log(Data)
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
 
 
 // basically telling it where to look at
@@ -14,64 +11,12 @@ module.exports = {
 // used "entry: './src'" instead of "entry: './src/index.js"
 // it worked but not sure yet if I can just use a dir name instead of a file 
 // to bundle all the files inside a directory into a file like "main.js" in this case
-  entry: './src/index.js',
+entry: {
+  index: './src/index.js',
+  print: './src/print.js',
+},
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-// added this part after installing css loader and style loader
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        // they are loaded in the reverse order and the last one (first one in the array)
-        // is expected to be javascript so the order in which you add them in the array matter
-        use: ['style-loader', 'css-loader'],
-        // This enables you to import './style.css' into the file that depends on that styling. 
-        // Now, when that module is run, a <style> tag with the stringified css will be inserted 
-        // into the <head> of your html file.
-      },
-      {
-        // adding this config allowed me to use a png file (as background) in this project
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        // adding this allowed me to use fonts 
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        // well names are pretty self explanatory but yeah this one lets me work with csv files
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        // and this one is for xml and so on
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
   },
 };
