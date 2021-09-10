d98c003c50b29ec0c64445569ca55e727480d8be
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-10 23:26:25 +0300

    implemented webpack --watch

--- src/print.js
+++ src/print.js
@@ -1,5 +1,4 @@
 export default function printMe() {
-    // console.log('I get called from print.js!');
-    // need an error to check how source map works
-    cosnole.log('I get called from print.js!');
+    console.log('I get called from print.js!');
+    
   }
